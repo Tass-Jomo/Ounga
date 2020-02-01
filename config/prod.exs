69bad9d6a933bef3ed4e8c103e07dbcdcba47c34
Crafty-Tass <tasmondi@gmail.com>
2020-02-01 14:08:48 +0300

    editing config file.

--- config/prod.exs
+++ config/prod.exs
@@ -11,10 +11,9 @@ use Mix.Config
 # before starting your production server.
 config :ounga, OungaWeb.Endpoint,
 	url: [host: "example.com", port: 80],
-    cache_static_manifest: "priv/static/cache_manifest.json",
     secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 # Do not print debug messages in production
-config :logger, level: :info
+     config :logger, level: :info
 
 
 config :ounga, Ounga.Repo,
